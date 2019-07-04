bbede4a7ab7733531c740beba47ca2bb4e27a283
kumarpintu2025 <41412742+kumarpintu2025@users.noreply.github.com>
2019-07-04 10:12:50 +0530

    Update and rename set5.py to set6.py

--- set5.py
+++
@@ -1,7 +0,0 @@
-x,y,z = map(int,input().split())
-if x>y and x>z:
-  print(x)
-elif y>x and y>z:
-  print(y)
-else:
-  print(z)
---
+++ set6.py
@@ -0,0 +1,5 @@
+w = int(input())
+if w%4==0:
+  print("yes")
+else:
+  print("no")
